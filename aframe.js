c5a0ef22813aecbbbb30a004eb846d759a194685
anrattanasouk <anrattanasouk@gmail.com>
2017-09-15 16:42:31 -0700

    changing images and sizing

--- aframe.js
+++ aframe.js
@@ -1,47 +1,87 @@
-AFRAME.registerComponent('set-image', {
+AFRAME.registerComponent('href', {
   schema: {
-    on: {type: 'string'},
-    target: {type: 'selector'},
-    src: {type: 'string'},
-    dur: {type: 'number', default: 300}
+    default: ''
   },
 
-  init: function () {
-    var data = this.data;
-    var el = this.el;
+  boundClickHandler: undefined,
 
-    this.setupFadeAnimation();
-
-    el.addEventListener(data.on, function () {
-      // Fade out image.
-      data.target.emit('set-image-fade');
-      // Wait for fade to complete.
-      setTimeout(function () {
-        // Set image.
-        data.target.setAttribute('material', 'src', data.src);
-      }, data.dur);
-    });
+  clickHandler: function hrefClickHandler() {
+    var url = this.data;
+    var target = this.el.getAttribute('target');
+    console.log('link to ' + url);
+    if (url && url[0] === '#') { // in-page anchor
+      var ele = document.querySelector(url);
+      var cams = document.querySelectorAll('a-camera');
+      if (ele && cams) {
+        var targetPosition = ele.getAttribute('position');
+        console.log('focus camera to position:' +
+          JSON.stringify(targetPosition));
+        cams[0].setAttribute('position', targetPosition);
+        window.location.hash = url;
+      } else {
+        console.log('#id or a-camera is not defined');
+      }
+    } else { // normal hyper link
+      if (target) {
+        var animation = '';
+        var exitAnimation = null;
+        console.log('target to ' + target);
+        if (target.indexOf('#') >= 0) {
+          var li = target.split('#');
+          target = li[0];
+          animation = li[1];
+          console.log('target to ' + target + ' & animate ' + animation);
+        }
+        switch(target) {
+        case '_blank':
+          if (animation) {
+            exitAnimation = document.getElementById(animation);
+            exitAnimation.addEventListener('animationend',
+              function animationendHandler() {
+                exitAnimation.removeEventListener('animationend',
+                  animationendHandler);
+                window.open(url);
+              });
+            this.el.emit('href');
+          } else {
+            window.open(url);
+          }
+          break;
+        case 'window':
+        default:
+          if (animation) {
+            exitAnimation = document.getElementById(animation);
+            exitAnimation.addEventListener('animationend',
+              function animationendHandler() {
+                exitAnimation.removeEventListener('animationend',
+                  animationendHandler);
+                window.location.href = url;
+              });
+            this.el.emit('href');
+          } else {
+            window.location.href = url;
+          }
+          break;
+        }
+      } else {
+        window.location.href = url;
+      }
+    }
   },
 
   /**
-   * Setup fade-in + fade-out.
+   * Called once when component is attached. Generally for initial setup.
    */
-  setupFadeAnimation: function () {
-    var data = this.data;
-    var targetEl = this.data.target;
+  init: function() {
+    this.boundClickHandler = this.clickHandler.bind(this);
+    this.el.addEventListener('click', this.boundClickHandler);
+  },
 
-    // Only set up once.
-    if (targetEl.dataset.setImageFadeSetup) { return; }
-    targetEl.dataset.setImageFadeSetup = true;
-
-    // Create animation.
-    targetEl.setAttribute('animation__fade', {
-      property: 'material.color',
-      startEvents: 'set-image-fade',
-      dir: 'alternate',
-      dur: data.dur,
-      from: '#FFF',
-      to: '#000'
-    });
+  /**
+   * Called when a component is removed (e.g., via removeAttribute).
+   * Generally undoes all modifications to the entity.
+   */
+  remove: function() {
+    this.el.removeEventListener('click', this.boundClickHandler);
   }
 });
